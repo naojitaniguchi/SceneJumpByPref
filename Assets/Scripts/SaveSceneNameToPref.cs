3087a8f1adf14eeaac3f86cd2af367711346aada
naojitaniguchi <naoji.taniguchi@gmail.com>
2021-05-19 11:59:23 +0900

    first commit

---
+++ Assets/Scripts/SaveSceneNameToPref.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SaveSceneNameToPref : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            saveSceneName();
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            clearPref();
+        }
+    }
+
+    public void saveSceneName()
+    {
+        string scenePath = SceneManager.GetActiveScene().path;
+        string sceneName = SceneManager.GetActiveScene().name;
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log(scenePath);
+        Debug.Log(sceneName);
+        Debug.Log(sceneIndex);
+
+        // SceneManager.GetSceneByBuildIndex(sceneIndex).name;
+
+        
+        PlayerPrefs.SetInt("LastSceneIndex", sceneIndex);
+        PlayerPrefs.Save();
+    }
+
+    public void clearPref()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
---
+++ Assets/Scripts/SceneSwitchByPref.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitchByPref : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int sceneIndex = -1;
+
+        if (PlayerPrefs.HasKey("LastSceneIndex"))
+        {
+            // 存在する
+            sceneIndex = PlayerPrefs.GetInt("LastSceneIndex", 0);
+
+            Debug.Log(sceneIndex);
+            SceneManager.LoadScene(sceneIndex);
+        }
+        else
+        {
+            // 存在しない
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
