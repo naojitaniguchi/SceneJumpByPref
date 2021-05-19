1bd91ea3f67fbe180039981a02a299da05c9213d
naojitaniguchi <naoji.taniguchi@gmail.com>
2021-05-19 20:02:25 +0900

    added Fungus collMethod sample.

--- Assets/Scripts/SceneSwitchByPref.cs
+++ Assets/Scripts/SceneSwitchByPref.cs
@@ -8,6 +8,22 @@ public class SceneSwitchByPref : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startFromStart()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+    public void startFromSavedScene()
+    {
         int sceneIndex = -1;
 
         if (PlayerPrefs.HasKey("LastSceneIndex"))
@@ -18,15 +34,5 @@ public class SceneSwitchByPref : MonoBehaviour
             Debug.Log(sceneIndex);
             SceneManager.LoadScene(sceneIndex);
         }
-        else
-        {
-            // 存在しない
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
