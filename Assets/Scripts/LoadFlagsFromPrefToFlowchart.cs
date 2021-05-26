d765a4e8036050eb4b6a7c8befb56db7e9ae1d11
naojitaniguchi <naoji.taniguchi@gmail.com>
2021-05-26 11:55:13 +0900

    added script for save flags.

---
+++ Assets/Scripts/LoadFlagsFromPrefToFlowchart.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadFlagsFromPrefToFlowchart : MonoBehaviour
+{
+    public GameObject targetFlow;
+    public string[] boolFlags;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadFlags()
+    {
+        for (int i = 0; i < boolFlags.Length; i++)
+        {
+            string flag = PlayerPrefs.GetString(boolFlags[i], "False");
+            if ( flag == "True")
+            {
+                targetFlow.GetComponent<Fungus.Flowchart>().SetBooleanVariable(boolFlags[i], true);
+            }
+            else
+            {
+                targetFlow.GetComponent<Fungus.Flowchart>().SetBooleanVariable(boolFlags[i], true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SaveFlags.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveFlags : MonoBehaviour
+{
+    public GameObject targetFlow;
+    public string[] boolFlags;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void saveFlagsFromFungus()
+    {
+        for (int i = 0; i < boolFlags.Length; i++)
+        {
+            bool flag = targetFlow.GetComponent<Fungus.Flowchart>().GetBooleanVariable(boolFlags[i]);
+            Debug.Log(boolFlags[i]);
+            Debug.Log(flag);
+
+            string prefValue = "False";
+            if (flag)
+            {
+                prefValue = "True";
+            }
+            PlayerPrefs.SetString(boolFlags[i], prefValue);
+            PlayerPrefs.Save();
+        }
+    }
+}
