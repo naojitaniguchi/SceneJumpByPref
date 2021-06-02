0d314f379861ea7496bd6934471f990c2d8dbfbe
naojitaniguchi <naoji.taniguchi@gmail.com>
2021-06-02 11:50:12 +0900

    added saving integer parameters.

---
+++ Assets/Scripts/LoadIntegersFromPrefToFlowchart.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadIntegersFromPrefToFlowchart : MonoBehaviour
+{
+    public GameObject targetFlow;
+    public string[] intVariables;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadIntegers()
+    {
+        for (int i = 0; i < intVariables.Length; i++)
+        {
+            int value = PlayerPrefs.GetInt(intVariables[i], -1);
+
+            Debug.Log(intVariables[i]);
+            Debug.Log(value);
+
+            targetFlow.GetComponent<Fungus.Flowchart>().SetIntegerVariable(intVariables[i], value);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/SaveIntegers.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveIntegers : MonoBehaviour
+{
+    public GameObject targetFlow;
+    public string[] intVariables;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void saveIntVariablesFromFungus()
+    {
+        for (int i = 0; i < intVariables.Length; i++)
+        {
+            int intValue = targetFlow.GetComponent<Fungus.Flowchart>().GetIntegerVariable(intVariables[i]);
+            Debug.Log(intVariables[i]);
+            Debug.Log(intValue);
+
+            PlayerPrefs.SetInt(intVariables[i], intValue);
+            PlayerPrefs.Save();
+        }
+    }
+}
